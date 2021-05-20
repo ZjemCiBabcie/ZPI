3a415d36ca37def6e8d1c1f8cbd0c92ac4abc0b6
ZjemCiBabcie <61703808+ZjemCiBabcie@users.noreply.github.com>
2021-05-20 20:20:16 +0200

    improve export from matlab

--- ZPI_Z3/writing.m
+++ ZPI_Z3/writing.m
@@ -1,6 +1,5 @@
-load('G:\My Drive\PWR\ZPI\ZPI\ZPI_Z3\opolska-brzezina.mat');
-
-writetimetable(Acceleration, 'G:\My Drive\PWR\ZPI\ZPI\ZPI_Z3\opolska-brzezina.xlsx', 'Sheet', 'Acceleration');
-writetimetable(AngularVelocity, 'G:\My Drive\PWR\ZPI\ZPI\ZPI_Z3\opolska-brzezina.xlsx',  'Sheet', 'AngularVelocity');
-writetimetable(Orientation, 'G:\My Drive\PWR\ZPI\ZPI\ZPI_Z3\opolska-brzezina.xlsx',  'Sheet', 'Orientation');
-writetimetable(Position, 'G:\My Drive\PWR\ZPI\ZPI\ZPI_Z3\opolska-brzezina.xlsx',  'Sheet', 'Position');    
+load('G:\My Drive\PWR\ZPI\ZPI\ZPI_Z3\siedzenie_przodem.mat');   
+% Acceleration(:, 1:end-1)
+% tt = synchronize(Acceleration(:, :-1), Orientation(:, 2:3), AngularVelocity(:, 2:3), Position, 'regular','linear','TimeStep',seconds(1))
+tt = synchronize(Acceleration(:, 1:end-1),'regular', 'linear', 'TimeStep',seconds(1))
+writetimetable(tt, 'G:\My Drive\PWR\ZPI\ZPI\ZPI_Z3\siedzenie_przodem.csv')
