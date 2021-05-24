7b3bb66072b721c009d10a125f514eafe24a1384
ZjemCiBabcie <61703808+ZjemCiBabcie@users.noreply.github.com>
2021-05-24 21:01:40 +0200

    change matlab export

--- ZPI_Z3/writing.m
+++ ZPI_Z3/writing.m
@@ -1,5 +1,7 @@
-load('G:\My Drive\PWR\ZPI\ZPI\ZPI_Z3\siedzenie_przodem.mat');   
-% Acceleration(:, 1:end-1)
-% tt = synchronize(Acceleration(:, :-1), Orientation(:, 2:3), AngularVelocity(:, 2:3), Position, 'regular','linear','TimeStep',seconds(1))
+load('G:\My Drive\PWR\ZPI\ZPI\ZPI_Z3\training-set.mat')
 tt = synchronize(Acceleration(:, 1:end-1),'regular', 'linear', 'TimeStep',seconds(1))
-writetimetable(tt, 'G:\My Drive\PWR\ZPI\ZPI\ZPI_Z3\siedzenie_przodem.csv')
+writetimetable(tt, 'G:\My Drive\PWR\ZPI\ZPI\ZPI_Z3\training-set.csv')
+
+load('G:\My Drive\PWR\ZPI\ZPI\ZPI_Z3\test-set.mat')
+tt = synchronize(Acceleration(:, 1:end-1),'regular', 'linear', 'TimeStep',seconds(1))
+writetimetable(tt, 'G:\My Drive\PWR\ZPI\ZPI\ZPI_Z3\test-set.csv')
