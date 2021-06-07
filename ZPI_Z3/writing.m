c6cf4fade2de303ec4eb8a73cada5ef201ef9c32
ZjemCiBabcie <61703808+ZjemCiBabcie@users.noreply.github.com>
2021-06-07 12:40:52 +0200

    change matlab code

--- ZPI_Z3/writing.m
+++ ZPI_Z3/writing.m
@@ -1,7 +1,9 @@
+clc
+clear
 load('G:\My Drive\PWR\ZPI\ZPI\ZPI_Z3\training-set.mat')
-tt = synchronize(Acceleration(:, 1:end-1),'regular', 'linear', 'TimeStep',seconds(1))
+tt = synchronize(Acceleration(:, 1:end-1),'regular', 'linear', 'TimeStep',seconds(1));
 writetimetable(tt, 'G:\My Drive\PWR\ZPI\ZPI\ZPI_Z3\training-set.csv')
 
 load('G:\My Drive\PWR\ZPI\ZPI\ZPI_Z3\test-set.mat')
-tt = synchronize(Acceleration(:, 1:end-1),'regular', 'linear', 'TimeStep',seconds(1))
+tt = synchronize(Acceleration(:, 1:end-1),'regular', 'linear', 'TimeStep',seconds(1));
 writetimetable(tt, 'G:\My Drive\PWR\ZPI\ZPI\ZPI_Z3\test-set.csv')
